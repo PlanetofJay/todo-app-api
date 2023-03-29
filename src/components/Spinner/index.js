db4a591d8d9b82fb54b6fee630c678e61547da02
PlanetofJay <jae44dv@gmail.com>
2023-03-29 16:57:11 -0400

    add ReactLoading

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,9 +1,18 @@
+import ReactLoading from 'react-loading';
 import './styles.scss';
 
-export default function Spinner() {
+export default function Spinner({ text }) {
   return (
     <div className='loading-component'>
-      Loading...
+      <div className='content'>
+        <ReactLoading
+          type='spin'
+          color='#008080'
+          height={100}
+          width={100}
+        />
+        <span>{text}</span>
+      </div>
     </div>
   )
 }
